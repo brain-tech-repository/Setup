b29138f15550d41bbaea0da178ad8769b78c73d5
brain-tech-repository <prakashnisad69@gmail.com>
2026-01-28 12:37:56 +0530

    some changes

--- app/(puplic)/login/page.tsx
+++ app/(puplic)/login/page.tsx
@@ -1,5 +1,5 @@
 "use"
-import { GalleryVerticalEnd } from "lucide-react"
+import { GalleryVerticalEnd, Link } from "lucide-react"
 
 import { LoginForm } from "@/components/login-form"
 import { Suspense } from "react"
@@ -8,12 +8,12 @@ export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <Link href="#" className="flex items-center gap-2 self-center font-medium ">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
           </div>
           Acme Inc.
-        </a>
+        </Link>
          <Suspense fallback={<div>Loading...</div>}>
       <LoginForm />
     </Suspense>
